bc85b8c8323f62a2bce1ffef5a330896442d024e
Andrea <andrea.9940@gmail.com>
2013-02-26 14:02:43 +0100

    example updated

--- example.d
+++ example.d
@@ -5,35 +5,32 @@ import deimos.X11.X, deimos.X11.Xlib;
 import std.stdio;
 
 int main()
-{;
+{
 	Display *display = XOpenDisplay(null);
 	if (!display) {
 		writeln("XOpenDisplay failed");
 		return 1;
 	}
 
+	int screen = DefaultScreen(display);
+
 	Window window = XCreateSimpleWindow(
 		display,
-		DefaultRootWindow(display),        	//parent window
-		0, 0, 323, 200,						//x, y, width, height
-		0, 0, 0xFF_FF_FF                	//border_width, boder_color, back_color
+		DefaultRootWindow(display),		//parent window
+		0, 0, 323, 200, 0,				//x, y, width, height, border_width
+		BlackPixel(display, screen),	//border_color
+		WhitePixel(display,screen)		//back_color
 	);
 
-    XMapWindow(display, window);
-    XRaiseWindow(display, window);
     XStoreName(display, window, cast(char*) "Example window");
-	XSelectInput(display, window, EventMask.ExposureMask);
-
-	XGCValues values;
-	GC gc = XCreateGC(display,
-		window,
-		GCMask.GCForeground,
-		&values
-	);
-
-	Atom wmDeleteMessage = XInternAtom(display, cast(char*) "WM_DELETE_WINDOW", Bool.False);
+	XSelectInput(display, window, ExposureMask);
+	XMapWindow(display, window);
+	
+	Atom wmDeleteMessage = XInternAtom(display, cast(char*) "WM_DELETE_WINDOW", False);
 	XSetWMProtocols(display, window, &wmDeleteMessage, 1);
 
+	GC gc = DefaultGC(display, screen);
+	
 	bool running = true;
 	while (running) {
 		XEvent event;
@@ -42,11 +39,11 @@ int main()
 			XNextEvent(display, &event);
 			switch (event.type)
 			{
-			case EventType.ClientMessage:
+			case ClientMessage:
 				running = false;
 				break;
-			case EventType.Expose:
-				XDrawString(display, window, gc, 10, 16, cast(char*)"Hello world !", 13);
+			case Expose:
+				XDrawString(display, window, gc, 10, 16, cast(char*) "Hello world !", 13);
 				break;
 			default:
 			}
